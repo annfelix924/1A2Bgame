fdf4949bbe1778948646c8d07216197676f86e85
annfelix <a1314131413141314@gmail.com>
2023-02-14 11:36:29 +0800

    加入專案檔案。

---
+++ 1A2B-2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1A2B_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("歡迎來到 1A2B 猜數字的遊戲～");
+
+            Random random = new Random();
+            int[] answer = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                int num = random.Next(0, 10);
+                if (Array.IndexOf(answer, num) == -1)
+                {
+                    answer[i] = num;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("請輸入 4 個數字：");
+                string input = Console.ReadLine();
+
+                int A = 0;
+                int B = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    int num = input[i] - '0';
+                    if (Array.IndexOf(answer, num) != -1)
+                    {
+                        if (answer[i] == num)
+                        {
+                            A++;
+                        }
+                        else
+                        {
+                            B++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("判定結果是 " + A + "A" + B + "B");
+                if (A == 4)
+                {
+                    Console.WriteLine("恭喜你！猜對了！！");
+                    break;
+                }
+
+                Console.WriteLine("------");
+            }
+
+            Console.Write("你要繼續玩嗎？(y/n): ");
+            string playAgain = Console.ReadLine();
+            if (playAgain.ToLower() == "n")
+            {
+                Console.WriteLine("遊戲結束，下次再來玩喔～");
+            }
+            else
+            {
+                Main(null);
+            }
+        }
+    }
+}
+
---
+++ 1A2B/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1A2B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("歡迎來到 1A2B 猜數字的遊戲～");
+            Random random = new Random();
+            int[] answer = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                int number = random.Next(0, 10);
+                while (Array.IndexOf(answer, number) != -1)
+                {
+                    number = random.Next(0, 10);
+                }
+                answer[i] = number;
+            }
+
+            while (true)
+            {
+                Console.Write("請輸入 4 個數字：");
+                string input = Console.ReadLine();
+                if (input.Length != 4)
+                {
+                    Console.WriteLine("輸入格式錯誤，請重新輸入！");
+                    continue;
+                }
+
+                int A = 0;
+                int B = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    int number = int.Parse(input[i].ToString());
+                    if (answer[i] == number)
+                    {
+                        A++;
+                    }
+                    else if (Array.IndexOf(answer, number) != -1)
+                    {
+                        B++;
+                    }
+                }
+
+                Console.WriteLine("判定結果是 " + A + "A" + B + "B");
+                if (A == 4)
+                {
+                    Console.WriteLine("恭喜你！猜對了！！");
+                    break;
+                }
+
+                Console.Write("你要繼續玩嗎？(y/n): ");
+                string continueGame = Console.ReadLine();
+                if (continueGame != "y")
+                {
+                    Console.WriteLine("遊戲結束，下次再來玩喔～");
+                    break;
+                }
+            }
+
+        }
+    }
+}
